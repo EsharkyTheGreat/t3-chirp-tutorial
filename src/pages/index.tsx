c81162f081d63b67d2ef567dc54adfc7d7922846
EsharkyTheGreat <eshwars1210@gmail.com>
2023-04-13 13:16:25 +0530

    New Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { type NextPage } from "next";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
+import { api } from "~/utils/api"
+
 const Home: NextPage = () => {
+  const { data } = api.posts.getAll.useQuery();
+
 
   const user = useUser();
   return (
@@ -9,6 +13,9 @@ const Home: NextPage = () => {
       <div>
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
+      </div>
+      <div>
+        {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
       </div>
     </>
   );
